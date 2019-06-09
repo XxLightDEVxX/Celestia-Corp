a293dafaab1a8f9aa4afd6abdae9f1566977a8cc
XxLightDEVxX <43815439+XxLightDEVxX@users.noreply.github.com>
2019-06-09 20:25:00 +0200

    Update cmd.js

--- cmd.js
+++ cmd.js
@@ -1,1 +1,23 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+const botconfig = JSON.parse(fs.readFileSync("jsonstorage/botconfig.json", "utf8"));
+const coinfile = JSON.parse(fs.readFileSync("jsonstorage/money.json", "utf8"));
+
+/////////////////////////////////
+//////////BALANCE CMD////////////
+/////////////////////////////////
+
+module.exports.run = async (bot, message, args) => {
+
+  /* Votre Code ici */
+
+}
+
+/////////////////////////////////
+/////////////NOM CMD/////////////
+/////////////////////////////////
+
+module.exports.help = {
+    name: "nomdelacommande"
+}
 
